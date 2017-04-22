487815f7e09dc2637f2f1ed326ed9a447fd937d5
linhng15 <nguyett@lakeforest.edu>
2017-04-22 09:07:13 -0500

    javascript

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,47 +3,80 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-	host: "localhost",
-	port:3306,
-	user: "root",
-	password: "password",
-	database: "Bamazon_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "Bamazon_db"
 });
 
-connection.connect(function(error){
-	if(error) {
-		console.log("getting the error message inside connection.connect");
-	console.log(err); 
-	} else{
-		console.log("connect as id" + connection.threadId);
-		console.log("Items available for sale!!!!")
-	}
+connection.connect(function(error) {
+  if (error) {
+    console.log("getting the error message inside connection.connect");
+    console.log(err);
+  } else {
+    console.log("connect as id" + connection.threadId);
+  }
 });
 
-connection.query("SELECT * FROM products", function(error, response){
-	// console.log(response);
-	for (var i = 0; i < response.length; i++) {
-		console.log("ID: " +response[i].item_id + " ||Product: " + response[i].product_name + " ||Department: " + response[i].department_name + " ||Price: " + response[i].price + " ||Quantity: " + response[i].stock_quantity);
-	}
-	console.log("__________________________________________");
-});
-
-var customerChoice = function(){
-	inquirer.prompt({
-		name: "customer",
-		type: "input",
-		message: "Please input the ID of the product you would like to buy."
-	}).then
+var display = function() {
+  connection.query("SELECT * FROM products", function(error, response) {
+    // console.log(response);
+    for (var i = 0; i < response.length; i++) {
+      console.log("ID: " + response[i].item_id + " ||Product: " + response[i].product_name + " ||Department: " + response[i].department_name + " ||Price: " + response[i].price + " ||Quantity: " + response[i].stock_quantity);
+    }
+    console.log("__________________________________________");
+    console.log("Our Inventory!")
+    customerDecision();
+  });
 };
 
-/*Then create a Node application called bamazonCustomer.js. 
 
-The app should then prompt users with two messages.
-The first should ask them the ID of the product they would like to buy.
-The second message should ask how many units of the product they would like to buy.
-Once the customer has placed the order, 
-your application should check if your store has enough of the product to meet the customer's request.
-If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-However, if your store does have enough of the product, you should fulfill the customer's order.
-This means updating the SQL database to reflect the remaining quantity.
-Once the update goes through, show the customer the total cost of their purchase. */
+var customerDecision = function() {
+    connection.query("SELECT * FROM products"),
+      function(error, results) {
+        if (error) throw error;
+      }
+    inquirer.prompt([{
+        name: "choice",
+        type: "rawlist",
+        choices: function() {
+          var choiceArray = [];
+          for (var i = 0; i < results.length; i++) {
+            choiceArray.push(results[i].item_id);
+          }
+          return choiceArray;
+        },
+        message: "Please input the ID of the product you would like to buy."
+      }]).then(function(answer) {
+          var chosenItem;
+          for (var i = 0; i < results.length; i++) {
+            if (results[i].item_id === answer.choice) {
+              chosenItem = results[i];
+              inquirer.prompt([{
+                name: "stock_quantity",
+                type: "input",
+                message: "how many units of the product would you like to buy?",
+                validate: function(value) {
+                  if (isNaN(value) === false) {
+                    return true;
+                  }
+                  return false;
+                }
+              }]).then(function(answer) {
+                  //determine if stock quantity < 0 / insufficient or minus the user input
+                  if ((product.stock_quantity - chosenItem.stock_quantity) > 0) {
+                    connection.query("UPDATE products SET stock_quantity='" + (product.stock_quantity - chosenItem.stock_quantity), function(error, results2) {
+                      console.log('Product Bought');
+                      display();
+                    });
+                  } else {
+                    console.log("Insufficient quantity!");
+                  }
+                });
+            }
+        }
+    });
+};
+
+display();
